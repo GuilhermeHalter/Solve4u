6dd361d7fa9d0361964ac6d08e9cdc9657ebae8b
GuilhermeHalter <halter.guilhermenunes@gmail.com>
2024-04-21 10:36:10 -0300

    Create component  HeaderHome

---
+++ Frontend-React/solve4u-react/src/components/home/HomeHeaderComp.jsx
@@ -0,0 +1,30 @@
+import "./HeaderComp.css"
+import { Link } from "react-router-dom";
+import { IoSearchOutline } from "react-icons/io5";
+
+const HomeHeader = () => {
+    return (
+        <nav className="navbar">
+        <ul>
+          <form className="formItem">
+            <input type="text" 
+            placeholder="Busque aqui" 
+            />
+            <button type="submit">
+            <IoSearchOutline />
+            </button>
+          </form>
+          
+          <li>
+          <Link to={"/"} className="Aheader">Sing Up</Link>
+          </li>
+          <li>
+          <Link to={"/"} className="Aheader">Sing In</Link>
+          </li>
+          
+        </ul>
+      </nav>
+      )
+  }
+  
+  export default HomeHeader;
