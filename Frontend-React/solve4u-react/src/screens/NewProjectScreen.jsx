4734a00dcaba0ed2fa4d1bf1fabd4054dbd7a4b8
Guilherme Halter Nunes <halter.guilhermenunes@gmail.com>
2024-04-22 22:11:41 -0300

    Create "newProjectScreen"

--- Frontend-React/solve4u-react/src/screens/NewProjectScreen.jsx
+++ Frontend-React/solve4u-react/src/screens/NewProjectScreen.jsx
@@ -1,8 +1,20 @@
+import "../css/NewProject.css"
+
 const NewProject = () => {
     return (
-        <div>
-            <h1>Create New Project</h1>
-        </div>
+    <div>
+            <h1>You don't have any project create one!!</h1>
+            <div className="joinProjectCard">
+              <p>  Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde at dicta, delectus reiciendis vel pariatur. Eligendi labore magnam similique nobis eaque maxime cupiditate, numquam sint quibusdam iusto molestiae alias libero.
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde tenetur nostrum nesciunt. Magnam enim pariatur repellendus aliquid voluptatem alias libero, consectetur illo labore dolorum amet harum deleniti id quod dolores?</p>
+                <button className="joinCardButton">Join Project</button>
+            </div>
+            <div className="createProjectCard">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde at dicta, delectus reiciendis vel pariatur. Eligendi labore magnam similique nobis eaque maxime cupiditate, numquam sint quibusdam iusto molestiae alias libero.
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde tenetur nostrum nesciunt. Magnam enim pariatur repellendus aliquid voluptatem alias libero, consectetur illo labore dolorum amet harum deleniti id quod dolores?
+            <button>Create Project</button>
+            </div>
+    </div>
     );
 
 }
