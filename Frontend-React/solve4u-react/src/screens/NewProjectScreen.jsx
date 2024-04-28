fb6bcc5e244433c034085b4835fb42d20600182e
GuilhermeHalter <halter.guilhermenunes@gmail.com>
2024-04-28 17:38:05 -0300

    Leaving Responsive Layout

--- Frontend-React/solve4u-react/src/screens/NewProjectScreen.jsx
+++ Frontend-React/solve4u-react/src/screens/NewProjectScreen.jsx
@@ -1,26 +1,33 @@
-import "../css/NewProject.css"
+// NewProjectScreen.jsx
+import React from 'react';
+import "../css/NewProject.css";
+import Sidebar from "../components/global/SidebarComp.jsx";
 
 const NewProject = () => {
-    return (
-    <div>
-          <h1>You don't have any project create one!!</h1>
-          <div className="cardsContainer">
-            <div className="cardsRow">          
-            <div className="joinProjectCard">
-              <p>  Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde at dicta, delectus reiciendis vel pariatur. Eligendi labore magnam similique nobis eaque maxime cupiditate, numquam sint quibusdam iusto molestiae alias libero.
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde tenetur nostrum nesciunt. Magnam enim pariatur repellendus aliquid voluptatem alias libero, consectetur illo labore dolorum amet harum deleniti id quod dolores?</p>
-                <button className="joinCardButton">Join Project</button>
-            </div>
-            <div className="createProjectCard">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde at dicta, delectus reiciendis vel pariatur. Eligendi labore magnam similique nobis eaque maxime cupiditate, numquam sint quibusdam iusto molestiae alias libero.
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde tenetur nostrum nesciunt. Magnam enim pariatur repellendus aliquid voluptatem alias libero, consectetur illo labore dolorum amet harum deleniti id quod dolores?
-            <button className="createCardButton">Create Project</button>
-            </div>
-            </div>
-            </div>
+  return (
+    <div className="new-project-container">
+      <h1>You don't have any projects yet. Create one!</h1>
+      <Sidebar />
+      <div className="cardsContainer">
+        <div className="cardsRow">          
+          <div className="joinProjectCard">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde at dicta, delectus reiciendis vel pariatur. Eligendi labore magnam similique nobis eaque maxime cupiditate, numquam sint quibusdam iusto molestiae alias libero.
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde tenetur nostrum nesciunt. Magnam enim pariatur repellendus aliquid voluptatem alias libero, consectetur illo labore dolorum amet harum deleniti id quod dolores?
+            </p>
+            <button className="cardButtonJoin">Join Project</button>
+          </div>
+          <div className="createProjectCard">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde at dicta, delectus reiciendis vel pariatur. Eligendi labore magnam similique nobis eaque maxime cupiditate, numquam sint quibusdam iusto molestiae alias libero.
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde tenetur nostrum nesciunt. Magnam enim pariatur repellendus aliquid voluptatem alias libero, consectetur illo labore dolorum amet harum deleniti id quod dolores?
+            </p>
+            <button className="cardButtonCreate">Create Project</button>
+          </div>
+        </div>
+      </div>
     </div>
-    );
-
+  );
 }
 
 export default NewProject;
