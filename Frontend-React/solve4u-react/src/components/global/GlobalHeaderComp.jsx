15618bc734b6945ed49a3dfe04cb1d1b93ccad3b
WedleySilva <WedleySilva2105@gmail.com>
2024-04-29 20:13:37 -0300

    Create "GlobalHeaderComp"

---
+++ Frontend-React/solve4u-react/src/components/global/GlobalHeaderComp.jsx
@@ -0,0 +1,13 @@
+import "../../css/GlobalHeaderComp.css"
+import { Link } from "react-router-dom";
+import { IoSearchOutline } from "react-icons/io5";
+
+const GlobalHeader = () => {
+    return (
+        <nav className="navbar">
+            <h1 className="logo">Solve<span className="four">4</span>u</h1>
+      </nav>
+      )
+  }
+  
+  export default GlobalHeader;
--- Frontend-React/solve4u-react/src/screens/NewProjectScreen.jsx
+++ Frontend-React/solve4u-react/src/screens/NewProjectScreen.jsx
@@ -1,33 +1,37 @@
-// NewProjectScreen.jsx
 import "../css/NewProject.css";
 import Sidebar from "../components/global/SidebarComp.jsx";
+import GlobalHeader from "../components/global/GlobalHeaderComp.jsx"
 import { FaArrowRight, FaPlus  } from "react-icons/fa";
 
 
 const NewProject = () => {
   return (
-    <div className="new-project-container">
-      <h1>You don't have any projects yet. Create one!</h1>
-      <Sidebar />
-      <div className="cardsContainer">
-        <div className="cardsRow">          
-          <div className="joinProjectCard">
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde at dicta, delectus reiciendis vel pariatur. Eligendi labore magnam similique nobis eaque maxime cupiditate, numquam sint quibusdam iusto molestiae alias libero.
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde tenetur nostrum nesciunt. Magnam enim pariatur repellendus aliquid voluptatem alias libero, consectetur illo labore dolorum amet harum deleniti id quod dolores?
-            </p>
-            <button className="cardButtonJoin">Join Project <FaArrowRight /></button>
-          </div>
-          <div className="createProjectCard">
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde at dicta, delectus reiciendis vel pariatur. Eligendi labore magnam similique nobis eaque maxime cupiditate, numquam sint quibusdam iusto molestiae alias libero.
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde tenetur nostrum nesciunt. Magnam enim pariatur repellendus aliquid voluptatem alias libero, consectetur illo labore dolorum amet harum deleniti id quod dolores?
-            </p>
-            <button className="cardButtonCreate">Create Project <FaPlus /></button>
+    <>
+    <GlobalHeader />
+      <div className="new-project-container">
+        <h1>You don't have any projects yet. Create one!</h1>
+        <Sidebar />
+        <div className="cardsContainer">
+          <div className="cardsRow">          
+            <div className="joinProjectCard">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde at dicta, delectus reiciendis vel pariatur. Eligendi labore magnam similique nobis eaque maxime cupiditate, numquam sint quibusdam iusto molestiae alias libero.
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde tenetur nostrum nesciunt. Magnam enim pariatur repellendus aliquid voluptatem alias libero, consectetur illo labore dolorum amet harum deleniti id quod dolores?
+              </p>
+              <button className="cardButtonJoin">Join Project <FaArrowRight className="icon" /></button>
+            </div>
+            <div className="createProjectCard">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde at dicta, delectus reiciendis vel pariatur. Eligendi labore magnam similique nobis eaque maxime cupiditate, numquam sint quibusdam iusto molestiae alias libero.
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde tenetur nostrum nesciunt. Magnam enim pariatur repellendus aliquid voluptatem alias libero, consectetur illo labore dolorum amet harum deleniti id quod dolores?
+              </p>
+              <button className="cardButtonCreate">Create Project <FaPlus className="icon"/></button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
+    
   );
 }
 
