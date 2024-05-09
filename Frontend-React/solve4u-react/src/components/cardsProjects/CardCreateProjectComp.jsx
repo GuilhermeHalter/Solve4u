c73c081185f615903c4d8cf0eebde7fa2997aece
GuilhermeHalter <guisam.halter@gmail.com>
2024-05-09 09:29:38 -0300

    Saving data to LocalStorage.

--- Frontend-React/solve4u-react/src/components/cardsProjects/CardCreateProjectComp.jsx
+++ Frontend-React/solve4u-react/src/components/cardsProjects/CardCreateProjectComp.jsx
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../css/CardCreateProjectComp.css";
 import { FaPlus } from "react-icons/fa";
 
-
 const CardCreateProject = ({ onClose }) => {
+  const [projectName, setProjectName] = useState("");
+  const [projectDescription, setProjectDescription] = useState("");
+  const [category, setCategory] = useState("Option 1");
+
+  const handleCreateProject = () => {
+    const projects = JSON.parse(localStorage.getItem("projects")) || [];
+
+    projects.push({ projectName, projectDescription, category });
+
+    localStorage.setItem("projects", JSON.stringify(projects));
+
+    setProjectName("");
+    setProjectDescription("");
+    setCategory("Option 1");
+  };
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    if (name === "projectName") {
+      setProjectName(value);
+    } else if (name === "projectDescription") {
+      setProjectDescription(value);
+    } else if (name === "category") {
+      setCategory(value);
+    }
+  };
+
   return (
     <div className="modal-background">
       <div className="modal">
@@ -11,16 +37,38 @@ const CardCreateProject = ({ onClose }) => {
           X
         </button>
         <h2>Create New Project</h2>
-        <input type="text" className="projectInput" placeholder="Project Name" />
-        <input type="text" placeholder="Project Description" className="description" />
-        <select name="category" id="category">
-          <option value="option 1">Option 1</option>
-          <option value="option 2">Option 2</option>
-          <option value="option 3">Option 3</option>
+        <input
+          type="text"
+          className="projectInput"
+          placeholder="Project Name"
+          name="projectName"
+          value={projectName}
+          onChange={handleInputChange}
+        />
+        <input
+          type="text"
+          placeholder="Project Description"
+          className="description"
+          name="projectDescription"
+          value={projectDescription}
+          onChange={handleInputChange}
+        />
+        <select
+          name="category"
+          id="category"
+          value={category}
+          onChange={handleInputChange}
+        >
+          <option value="Option 1">Option 1</option>
+          <option value="Option 2">Option 2</option>
+          <option value="Option 3">Option 3</option>
         </select>
-        <button className="create-btn">Create Project <FaPlus className="iconCard"/></button>
+        <button className="create-btn" onClick={handleCreateProject}>
+          Create Project <FaPlus className="iconCard" />
+        </button>
       </div>
     </div>
   );
 };
+
 export default CardCreateProject;
