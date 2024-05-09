d430ef22e13cf05a63e81bc25be679f198b0816d
GuilhermeHalter <guisam.halter@gmail.com>
2024-05-09 11:12:58 -0300

    Get data from LocalStorage

--- Frontend-React/solve4u-react/src/screens/ProjectsScreen.jsx
+++ Frontend-React/solve4u-react/src/screens/ProjectsScreen.jsx
@@ -1,9 +1,17 @@
+import React, { useEffect, useState } from 'react';
 import Sidebar from "../components/global/SidebarComp.jsx";
 import GlobalHeader from "../components/global/GlobalHeaderComp.jsx";
 import CardProject from "../components/cardsProjects/CardsProjectsComp.jsx";
 import "../css/ProjectsScreen.css"
 
 const Projects = () => {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    const localStorageProjects = JSON.parse(localStorage.getItem('projects')) || [];
+    setProjects(localStorageProjects);
+  }, []);
+
   return (
     <div>
       <GlobalHeader />
@@ -13,42 +21,17 @@ const Projects = () => {
           <h1>Seus Projetos</h1>
         </header>
         <div className="projects-container">
-          <CardProject
-            title="Projeto ipsum 1"
-            content="Lorem ipsum dolor sit amet consectetur."
-          />
-          <CardProject
-            title="Projeto ipsum 2"
-            content="Sed tortor quam iaculis ultrices."
-          />
-          <CardProject
-            title="Projeto ipsum 3"
-            content="Malesuada. Mollis purus magna phasellus."
-          />
-          <CardProject
-            title="Projeto ipsum 3"
-            content="Malesuada. Mollis purus magna phasellus."
-          />
+          {projects.map((project, index) => (
+            <CardProject
+              key={index}
+              title={project.projectName}
+              content={project.projectDescription}
+            />
+          ))}
         </div>
         <section>
           <h2 className="subtitle">Análises</h2>
           <div className="analytics-container">
-            <CardProject
-              title="Projeto 1"
-              content="Lorem ipsum dolor sit amet consectetur."
-            />
-            <CardProject
-              title="Projeto 2"
-              content="Sed tortor quam iaculis ultrices."
-            />
-            <CardProject
-              title="Projeto 3"
-              content="Malesuada. Mollis purus magna phasellus."
-            />
-            <CardProject
-              title="Projeto 3"
-              content="Malesuada. Mollis purus magna phasellus."
-            />
           </div>
         </section>
       </div>
