d7387030e77b91c98161b163156919a7a70d7f0d
WedleySilva <WedleySilva2105@gmail.com>
2024-05-08 21:00:24 -0300

    Edit HomeFooterComp

--- Frontend-React/solve4u-react/src/components/home/HomeFooterComp.jsx
+++ Frontend-React/solve4u-react/src/components/home/HomeFooterComp.jsx
@@ -1,14 +1,37 @@
 import "../../css/HomeFooterComp.css"
-import { Link } from "react-router-dom";
-import { IoSearchOutline } from "react-icons/io5";
-import { IoIosArrowDown } from "react-icons/io";
+import { FaInstagram } from "react-icons/fa";
+import { FaFacebookSquare } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
 
 const HomeFooter = () => {
     return (
-        
-<h1>Hello World</h1>
+        <div className="footerComp">
+            <div className="footerList">
+                <div className="contactLi">
+                    <h2 className="contactTitle">Contact</h2>
+                    <ul>
+                        <li><a target="_blank" href="#">solve4u@gmail.com</a></li>
+                    </ul>
+                </div>
+                <div className="helpLi">
+                    <h2 className="helpTitle" >Help</h2>
+                    <ul>
+                        <li><a href="#">Support</a></li>
+                        <li><a href="#">Tutorial</a></li>
+                        <li><a href="#">Order Status</a></li>
+                    </ul>
+                </div>
+                <div className="followUsLi">
+                    <h2 className="followUsTitle" >Follow Us</h2>
+                    <ul>
+                        <li><a target="_blank" href="https://www.facebook.com/"><FaFacebookSquare className="facebookIcon"/> Facebook</a></li>
+                        <li><a target="_blank" href="https://www.instagram.com/"><FaInstagram className="instagramIcon"/> Instagram</a></li>
+                        <li><a target="_blank" href="https://twitter.com"><FaXTwitter className="twitterIcon"/> Twitter</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
 
-        )
-    }
-    
-    export default HomeFooter;
+export default HomeFooter;
