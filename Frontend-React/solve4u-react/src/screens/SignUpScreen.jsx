e33bb121bad336ee597a1e96d37e5aed5bd6f586
WedleySilva <WedleySilva2105@gmail.com>
2024-05-17 21:54:34 -0300

    Edit SignIn and SignUp Screens

--- Frontend-React/solve4u-react/src/screens/SignUpScreen.jsx
+++ Frontend-React/solve4u-react/src/screens/SignUpScreen.jsx
@@ -1,27 +1,36 @@
 import { Link } from "react-router-dom";
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub, FaLinkedin, FaBookmark } from "react-icons/fa";
+import { FaArrowLeftLong } from "react-icons/fa6";
+import "../css/screenStyle/signScreen/SignUpScreen.css";
 
-const SignUp = () => {
+const SignIn = () => {
     return (
-        <div>
-            <div>
-                <h1>MANAGE YOUR JOB</h1>
-                <div className="manageJobText">
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus magnam temporibus, cupiditate quisquam repellat distinctio qui commodi unde esse, expedita, non voluptatibus. Animi, quo hic molestiae perferendis ex incidunt accusamus!
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt quod voluptatum harum, perferendis natus ad delectus iure tenetur quibusdam nihil autem recusandae molestiae at explicabo vitae earum soluta culpa. Officiis?
+        <div className="global">
+            <div className="signUpBackButtonComp">     
+            <Link to={"/"} className="signUpBackButton"><FaArrowLeftLong className="arrowBack"/> BACK </Link>
+            </div>
+            <div className="signUpContainer">
+                <div className="manageJobComp">
+                    <FaBookmark className="bookMarkIcon"/>
+                    <h1 className="cardTitle">WELCOME</h1>
+                    <p className="contentText"> We're here to streamline the way you manage your projects, offering powerful and intuitive tools to boost your productivity and achieve exceptional results.</p>
+                     
+                </div>
+                <div className="loginComp">
+                    <h1 className="loginTitle">LOGIN</h1>
+                    <form className="inputSignIn">
+                        <input type="text" className="nameInput" placeholder="Name" required />
+                        <input type="text" className="passwordInput" placeholder="Password" required/>
+                    </form>
+
+                    <div className="signInButtonComp">
+                        <Link to={"/newproject"} className="loginButton"> LOGIN </Link>
+                    </div>
                 </div>
             </div>
-            <div className="login">
-                <h1>LOGIN</h1>  
-                <div className="inputSignUp">
-                    <input type="text" className="email" placeholder="E-mail" />
-                    <input type="text" className="password" placeholder="Password" />
-                </div>
-                <Link to={"/newproject"}> LOGIN </Link>
-                </div>
-            </div>
+        </div>
     );
-
-
 }
 
-export default SignUp; 
+export default SignIn;
