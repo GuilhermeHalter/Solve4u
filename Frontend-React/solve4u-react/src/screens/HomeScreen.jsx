d8f8f118a15e6ab1b0f59fb20fb390542100922c
WedleySilva <WedleySilva2105@gmail.com>
2024-05-20 21:37:40 -0300

    Edit Home Screen text animation

--- Frontend-React/solve4u-react/src/screens/HomeScreen.jsx
+++ Frontend-React/solve4u-react/src/screens/HomeScreen.jsx
@@ -9,9 +9,11 @@ const Home = () => {
       <div className="home-container">
         <HomeHeader />
         <div className="welcomeText">
-          <h1>Welcome to</h1>
-          <h2>Solve<div className="four">4</div>u</h2>
-          <p>We're here to streamline the way you manage your projects, offering  powerful and intuitive tools to boost your productivity and achieve  exceptional results. Our commitment is to provide a platform that not  only organizes your tasks but also inspires collaboration, creativity,  and innovation at every stage of your project.</p>
+          <h1 className="titleAnimation">Welcome to</h1>
+          <h2 className="subTitleAnimation">Solve<div className="four">4</div>u</h2>
+          <div className="divHomeText">
+              <p className="homeText">We're here to streamline the way you manage your projects, offering  powerful and intuitive tools to boost your productivity and achieve  exceptional results. Our commitment is to provide a platform that not  only organizes your tasks but also inspires collaboration, creativity,  and innovation at every stage of your project.</p>
+          </div>
         </div>
       </div>
       <div className="section2" id="solutions">
