f53ab4fe43b1a51eb9e36a085b24bef96a2cb9b6
WedleySilva <WedleySilva2105@gmail.com>
2024-06-03 21:52:41 -0300

    Testing screen responsiveness

--- Frontend-React/solve4u-react/src/components/home/HomeHeaderComp.jsx
+++ Frontend-React/solve4u-react/src/components/home/HomeHeaderComp.jsx
@@ -1,5 +1,5 @@
-import "../../css/compStyle/compHomeStyle/HomeHeaderComp.css"
 import React from "react";
+import "../../css/compStyle/compHomeStyle/HomeHeaderComp.css"
 import { Link } from "react-router-dom";
 import { IoSearchOutline } from "react-icons/io5";
 import { IoIosArrowDown } from "react-icons/io";
