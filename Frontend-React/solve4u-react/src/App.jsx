92c80b422a2fb72f005fe8a7d4ebf21a97d844df
WedleySilva <WedleySilva2105@gmail.com>
2024-06-07 20:20:19 -0300

    loading animation

--- Frontend-React/solve4u-react/src/App.jsx
+++ Frontend-React/solve4u-react/src/App.jsx
@@ -1,16 +1,35 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import { Outlet } from 'react-router-dom';
+import { InfinitySpin } from 'react-loader-spinner';
 
 function App() {
+  const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
-    <>
-      <div className="container">
-        <Outlet/>
-      </div>
-    </>
-  )
+    <div className="containerApp">
+      {loading ? (
+        <div className="spinnerContainer">
+          <InfinitySpin
+            visible={true}
+            width="200"
+            color="#0076e2"
+            ariaLabel="infinity-spin-loading"
+          />
+        </div>
+      ) : (
+        <Outlet />
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
