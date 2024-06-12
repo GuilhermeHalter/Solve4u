fea848671a8aa48b7b72ce93d580cedc321787a9
WedleySilva <WedleySilva2105@gmail.com>
2024-06-12 19:37:02 -0300

    Responsiveness applied in SignInScreen and SignUpScreen

--- Frontend-React/solve4u-react/src/screens/SignInScreen.jsx
+++ Frontend-React/solve4u-react/src/screens/SignInScreen.jsx
@@ -5,28 +5,28 @@ import { FaArrowLeftLong } from "react-icons/fa6";
 import "../css/screenStyle/signScreen/SignInScreen.css";
 
 const SignIn = () => {
-    return (
-        <div className="global">
-            <div className="signInBackButtonComp">     
-            <Link to={"/"} className="signInBackButton"><FaArrowLeftLong className="signInArrowBack"/> BACK </Link>
-            </div>
-            <div className="signInContainer">
-                <div className="welcomeComp">
-                    <FaBookmark className="bookMarkIcon"/>
-                    <h1 className="welcomeTitle">WELCOME</h1>
-                    <p className="textContent"> We're here to streamline the way you manage your projects, offering powerful and intuitive tools to boost your productivity and achieve exceptional results.</p>
-                     
-                     <div className="signUpButtonComp">
-                        <Link to={"/signup"} className="signUpButton"> SIGN UP </Link>
-                     </div>
-                     
-                </div>
-                <div className="loginComp">
-                    <h1 className="loginTitle">LOGIN</h1>
-                    <form className="inputSignIn">
-                        <input type="email" className="inEmailInput" placeholder="E-mail" required />
-                        <input type="password" className="inPasswordInput" placeholder="Password" required/>
-                    </form>
+  return (
+    <div className="global">
+      <div className="signInBackButtonComp">
+        <Link to={"/"} className="signInBackButton"><FaArrowLeftLong className="signInArrowBack" /> BACK </Link>
+      </div>
+      <div className="signInContainer">
+        <div className="welcomeComp">
+          <FaBookmark className="bookMarkIcon" />
+          <h1 className="welcomeTitle">WELCOME</h1>
+          <p className="textContent"> We're here to streamline the way you manage your projects, offering powerful and intuitive tools to boost your productivity and achieve exceptional results.</p>
+
+          <div className="signUpButtonComp">
+            <Link to={"/signup"} className="signUpButton"> SIGN UP </Link>
+          </div>
+
+        </div>
+        <div className="loginComp">
+          <h1 className="loginTitle">LOGIN</h1>
+          <form className="inputSignIn">
+            <input type="email" className="inEmailInput" placeholder="E-mail" required />
+            <input type="password" className="inPasswordInput" placeholder="Password" required />
+          </form>
 
           <div className="signInButtonComp">
             <Link to={"/newproject"} className="singInButton">
