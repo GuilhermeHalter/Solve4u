726289fd15fd3851c39aa304a1571180917fbc36
GuilhermeHalter <guisam.halter@gmail.com>
2024-06-20 18:16:16 -0300

    Creating Comments on CardSectorComp for Documentation Purposes

--- Frontend-React/solve4u-react/src/components/cardsProjects/CardSectorComp.jsx
+++ Frontend-React/solve4u-react/src/components/cardsProjects/CardSectorComp.jsx
@@ -2,6 +2,29 @@ import React from "react";
 import PropTypes from "prop-types";
 import "../../css/cardStyle/CardSectorComp.css";
 
+/**
+ * Component for displaying a sector card.
+ *
+ * @component
+ * @example
+ * const sector = {
+ *   sectorName: "Sector 1",
+ *   sectorDescription: "This is a description",
+ *   selectedUsers: ["User 1", "User 2"],
+ *   sectorColor: "#ff0000"
+ * };
+ * const handleClick = (sector) => { console.log(sector); };
+ * return <SectorCard sector={sector} onClick={handleClick} />;
+ *
+ * @param {Object} props - Component properties.
+ * @param {Object} props.sector - The sector object containing details.
+ * @param {string} props.sector.sectorName - The name of the sector.
+ * @param {string} props.sector.sectorDescription - The description of the sector.
+ * @param {string[]} props.sector.selectedUsers - The users selected for the sector.
+ * @param {string} props.sector.sectorColor - The color associated with the sector.
+ * @param {Function} props.onClick - Function to call when the sector card is clicked.
+ */
+
 const SectorCard = ({ sector, onClick }) => {
   const { sectorName, sectorDescription, selectedUsers, sectorColor } = sector;
 
