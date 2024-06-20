6915cdcde639dbdb6c70a33cc28d595dde6a7ffe
GuilhermeHalter <guisam.halter@gmail.com>
2024-06-20 18:19:12 -0300

    Creating Comments on GlobalHeaderComp for Documentation Purposes

--- Frontend-React/solve4u-react/src/components/global/GlobalHeaderComp.jsx
+++ Frontend-React/solve4u-react/src/components/global/GlobalHeaderComp.jsx
@@ -2,6 +2,14 @@ import "../../css/compStyle/compGlobalStyle/GlobalHeaderComp.css";
 import { Link } from "react-router-dom";
 import { IoSearchOutline } from "react-icons/io5";
 
+/**
+ * GlobalHeader component renders the top navigation bar of the application.
+ *
+ * @component
+ * @example
+ * return <GlobalHeader />;
+ */
+
 const GlobalHeader = () => {
   return (
     <nav className="navbar">
