032d43eef99beaf008a818a70efb3ef4fcee39de
GuilhermeHalter <guisam.halter@gmail.com>
2024-06-20 18:17:10 -0300

    Creating Comments on CardProjectsComp for Documentation Purposes

--- Frontend-React/solve4u-react/src/components/cardsProjects/CardsProjectsComp.jsx
+++ Frontend-React/solve4u-react/src/components/cardsProjects/CardsProjectsComp.jsx
@@ -1,6 +1,22 @@
 import React from "react";
 import "../../css/cardStyle/CardsProjectComp.css";
 
+/**
+ * Component for displaying a project card.
+ *
+ * @component
+ * @example
+ * const project = { projectName: "Project 1", projectDescription: "This is a description" };
+ * const handleClick = (project) => { console.log(project); };
+ * return <CardProject project={project} onClick={handleClick} />;
+ *
+ * @param {Object} props - Component properties.
+ * @param {Object} props.project - The project object containing details.
+ * @param {string} props.project.projectName - The name of the project.
+ * @param {string} props.project.projectDescription - The description of the project.
+ * @param {Function} props.onClick - Function to call when the project card is clicked.
+ */
+
 const CardProject = ({ project, onClick }) => (
   <div className="card-comp" onClick={() => onClick(project)}>
     <h3>{project.projectName}</h3>
