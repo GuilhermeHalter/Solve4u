6ae5bbcad8e369d035405e40bd9e0579652fea37
GuilhermeHalter <guisam.halter@gmail.com>
2024-06-20 18:18:18 -0300

    Creating Comments on CardsSectorByProjectId for Documentation Purposes

--- Frontend-React/solve4u-react/src/components/cardsProjects/CardsSectorByProjectIdComp.jsx
+++ Frontend-React/solve4u-react/src/components/cardsProjects/CardsSectorByProjectIdComp.jsx
@@ -1,6 +1,18 @@
 import React from "react";
 import SectorCard from "../cardsProjects/CardSectorComp";
 
+/**
+ * Component for displaying a list of sectors filtered by project ID.
+ *
+ * @component
+ * @example
+ * const projectId = 1;
+ * return <CardsSectorByProjectId projectId={projectId} />;
+ *
+ * @param {Object} props - Component properties.
+ * @param {number} props.projectId - The ID of the project to filter sectors by.
+ */
+
 const CardsSectorByProjectId = ({ projectId }) => {
   const sectors = JSON.parse(localStorage.getItem("sectors")) || [];
 
