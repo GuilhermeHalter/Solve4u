8f1b7dea4c4aae2c8bccf42e11b0be3bea89af99
GuilhermeHalter <guisam.halter@gmail.com>
2024-07-01 13:40:00 -0300

    Adding ThemeContext.jsx

---
+++ Frontend-React/solve4u-react/src/assets/JavaScript/ThemeContext.jsx
@@ -0,0 +1,19 @@
+import React, { createContext, useState, useContext } from 'react';
+
+const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+
+  const toggleTheme = () => {
+    setIsDarkTheme(!isDarkTheme);
+  };
+
+  return (
+    <ThemeContext.Provider value={{ isDarkTheme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export const useTheme = () => useContext(ThemeContext);
