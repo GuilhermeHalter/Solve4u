4dba850a6f43655c5953f9b083504792b44abd64
GuilhermeHalter <guisam.halter@gmail.com>
2024-07-01 16:16:34 -0300

    Done Css Dark Theme Confirm Delete Sector

--- Frontend-React/solve4u-react/src/components/cardsProjects/CardDeleteSectorConfirmation.jsx
+++ Frontend-React/solve4u-react/src/components/cardsProjects/CardDeleteSectorConfirmation.jsx
@@ -1,13 +1,17 @@
 // CardDeleteSectorConfirmation.jsx
 import React from "react";
 import "../../css/cardStyle/CardDeleteSectorConfirmation.css";
+import { useTheme } from "../../assets/JavaScript/ThemeContext.jsx"; 
 
 const CardDeleteSectorConfirmation = ({ onConfirm, onCancel }) => {
+  const { isDarkTheme} = useTheme();
+
   return (
     <div className="modal-backgraundDelete">
-      <div className="delete-confirmation">
+      <div className={`delete-confirmation ${isDarkTheme ? 'dark' : 'light'}`}
+      >
         <h2>Are you sure you want to delete this sector?</h2>
-        <p>This action will delete all tasks related to this sector.</p>
+        <p>*This action will delete all tasks related to this sector.</p>
         <div className="button-container">
           <button className="confirm-button" onClick={onConfirm}>Confirm</button>
           <button className="cancel-button" onClick={onCancel}>Cancel</button>
